8318b4ec2e4e3e8e85b6c034ce0573ac128154cb
Aayush1225 <ayushacharya080@gmail.com>
2023-12-19 12:34:41 +0545

    q6 is added

---
+++ Q6Product.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+class Q6Product{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter first numver");
+        int a=sc.nextInt();
+        System.out.println("enter second numver");
+        int b=sc.nextInt();
+
+        int product=a*b;
+
+        System.out.println("the product is ::"+product);
+
+
+    }
+}
